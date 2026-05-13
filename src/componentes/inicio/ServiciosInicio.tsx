7f4ed6b56878b16c99cf9471276aaa7c5b42f687
Gianfranco Caccia <gianfrancocaccia@hotmail.com>
2026-05-13 19:55:18 -0300

    eepepee

--- src/componentes/inicio/ServiciosInicio.tsx
+++ src/componentes/inicio/ServiciosInicio.tsx
@@ -1,126 +1,48 @@
 import { Link } from "react-router-dom";
 
-
-
 function ServiciosInicio() {
-
   return (
-
     <section className="servicios-inicio">
 
-      <h2>
-        Nuestros Servicios
-      </h2>
+      <h2>Nuestros Servicios</h2>
 
       <div className="grid-servicios">
 
         {/* CIRUGÍA */}
-
         <Link to="/cirugia-plastica">
-
           <article className="tarjeta-servicio">
-
-                     <video
-              autoPlay
-              muted
-              loop
-              playsInline
-              className="video-servicio"
-               preload="none"
-         
-           
-            >
-
-              <source
-                src="/video.videoSiete.mp4"
-                type="video/mp4"
-              />
-
+            <video autoPlay muted loop playsInline className="video-servicio" preload="metadata">
+              <source src="/videos/videoUno.mp4" type="video/mp4" />
             </video>
-              
-            
-
-          
-
-            <h3>
-              Cirugía Plástica
-            </h3>
-
+            <h3>Cirugía Plástica</h3>
           </article>
-
         </Link>
 
         {/* BELLEZA FACIAL */}
-
         <Link to="/belleza-facial">
-
           <article className="tarjeta-servicio">
-
-            <video
-              autoPlay
-              muted
-              loop
-              playsInline
-              className="video-servicio"
-               preload="none"
-           
-            >
-
-              <source
-                src="/videos/videoOcho.mp4"
-                type="video/mp4"
-              />
-
+            <video autoPlay muted loop playsInline className="video-servicio" preload="metadata">
+              <source src="/videos/videoOcho.mp4" type="video/mp4" />
             </video>
-
-            <h3>
-              Medicina Estética
-            </h3>
-
+            <h3>Medicina Estética</h3>
           </article>
-
         </Link>
 
         {/* ODONTOLOGÍA */}
-
         <Link to="/odontologia">
-
           <article className="tarjeta-servicio">
-
-            <video
-              autoPlay
-              muted
-              loop
-              playsInline
-              className="video-servicio"
-               preload="none"
-           
-            >
-
-              <source
-                src="/videos/videoDos.mp4"
-                type="video/mp4"
-              />
-
+            <video autoPlay muted loop playsInline className="video-servicio" preload="metadata">
+              <source src="/videos/videoDos.mp4" type="video/mp4" />
             </video>
-
-            <h3>
-              Odontología
-            </h3>
-
+            <h3>Odontología</h3>
           </article>
-
         </Link>
- 
- 
+
       </div>
 
-      <Link
-  to="/servicios"
-  className="boton-turno"
->
-  Reserva tu turno
-</Link>
+      <Link to="/servicios" className="boton-turno">
+        Reserva tu turno
+      </Link>
 
     </section>
   );
